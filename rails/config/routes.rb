d2d32c6529ddccbedad37eece8ce7a5f0848016e
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-01-17 11:12:34 +0900

    テストツールのinstall

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get "health_check", to: "health_check#index"
+    end
+  end
 end
