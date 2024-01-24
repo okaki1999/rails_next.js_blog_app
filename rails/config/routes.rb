74eeb6fd433764eec6fbcc4b9531987d66b72c19
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-01-24 14:36:42 +0900

    モック作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
-
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
       namespace :current do
-        resource :user, only: [:show, :update]
+        resource :user, only: [:show]
         resources :articles, only: [:index, :show, :create, :update]
       end
+      resources :articles, only: [:index, :show]
     end
   end
 end
